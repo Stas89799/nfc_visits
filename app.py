eb20254480899021c7cad68ba7e4cadd6fe5b1b0
Stas89799 <89688847776@ya.ru>
2025-03-23 16:04:37 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, CardForm
+from models import User, Card
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!', 'success')
+        return redirect(url_for('create_card'))
+    return render_template('register.html', form=form)
+
+@app.route('/create_card', methods=['GET', 'POST'])
+def create_card():
+    form = CardForm()
+    if form.validate_on_submit():
+        card = Card(first_name=form.first_name.data, last_name=form.last_name.data, phone=form.phone.data)
+        db.session.add(card)
+        db.session.commit()
+        flash('Card created successfully!', 'success')
+        return redirect(url_for('view_card', card_id=card.id))
+    return render_template('card.html', form=form)
+
+@app.route('/card/<int:card_id>')
+def view_card(card_id):
+    card = Card.query.get_or_404(card_id)
+    return render_template('view_card.html', card=card)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    phone = db.Column(db.String(150), nullable=False)
+    photo = db.Column(db.String(150), nullable=True)
